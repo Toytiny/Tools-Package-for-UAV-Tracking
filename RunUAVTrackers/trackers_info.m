1fac85cee195128ac8307a72ceeb16b1b831a491
toytiny <2047952466@qq.com>
2020-02-02 16:32:14 +0800

    seventh update add OTB100

--- RunUAVTrackers/trackers_info.m
+++ RunUAVTrackers/trackers_info.m
@@ -1,6 +1,6 @@
 function info = trackers_info()
 info = {
-    struct('ECO_HC',@run_ECO_HC)
+    struct('DCF',@run_DCF)
 };
 %info结构体第一个字符串为tracker所在文件夹名称，亦为tracker的名称
 %info结构体第二个函数句柄为tracker运行的接口, 每个tracker文件夹内都应包含该文件
